0c236028272256156933bb358a91181e03ecce4f
Andreas Samuelsson <andreas.samuelsson@bonniernews.se>
2024-01-12 09:40:59 +0100

    Hello world skeleton

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!!!!' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
